2ae5d8519b72d5a550b07a43978079f1624cd372
Dhavalpaekh2610 <Dhavalparekh2610@gmail.com>
2024-12-27 17:45:41 +0530

    #6 Submitting Data with actions.

--- frontend/src/routes/NewPost.jsx
+++ frontend/src/routes/NewPost.jsx
@@ -1,69 +1,45 @@
-import React, { useState } from "react";
+import React from "react";
+import { Link, Form, redirect } from "react-router-dom";
 import NewPostModule from "./NewPost.module.css";
 import Modal from "../components/Modal";
-import { Link } from "react-router-dom";
 
-const NewPost = ({ onAddPost }) => {
-  const [enterBody, setEnterBody] = useState("");
-  const [enterAuthor, setEnterAuthor] = useState("");
-
-  const bodyChangeHandler = (event) => {
-    setEnterBody(event.target.value);
-  };
-
-  const authotChangeHandler = (event) => {
-    setEnterAuthor(event.target.value);
-  };
-  const cancleHandler = () => {
-    <Link to="/" />;
-  };
-  const submitHandler = (event) => {
-    event.preventDefault();
-    const postDate = {
-      body: enterBody,
-      author: enterAuthor,
-    };
-    onAddPost(postDate);
-    cancleHandler();
-  };
-
+const NewPost = () => {
   return (
     <Modal>
-      <form className={NewPostModule.form} onSubmit={submitHandler}>
+      <Form method="post" className={NewPostModule.form}>
         <p>
           <label htmlFor="body"> Text</label>
-          <textarea
-            htmlFor="body"
-            required
-            rows={4}
-            onChange={bodyChangeHandler}
-          />
+          <textarea id="body" name="body" required rows={4} />
         </p>
 
         <p>
           <label htmlFor="body">Your Name</label>
-          <textarea
-            htmlFor="body"
-            id="name"
-            required
-            onChange={authotChangeHandler}
-          />
+          <textarea id="name" name="author" required />
         </p>
         <p className={NewPostModule.action}>
           <Link to="/" className={NewPostModule.link} type="button">
             Cancle
           </Link>
-          <Link
-            to="/"
-            className={NewPostModule.link}
-            type="button"
-            onClick={submitHandler}
-          >
+          <button type="submit" className={NewPostModule.link}>
             Submit
-          </Link>
+          </button>
         </p>
-      </form>
+      </Form>
     </Modal>
   );
 };
 export default NewPost;
+
+export const action = async ({ request }) => {
+  const formData = await request.formData();
+  const postData = Object.fromEntries(formData); // { body: "text", name: "name" }
+  await fetch("http://localhost:8080/posts", {
+    method: "POST",
+    body: JSON.stringify(postData),
+    headers: {
+      "Content-Type": "application/json",
+    },
+  });
+
+  return redirect("/");
+};
